556b41a8b8d01a4bb438474a966e0c205b9048d7
mmso-o <4.mmsoo@gmail.com>
2023-02-19 17:05:03 +0900

    feat: 스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -10,21 +10,26 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// 애플리케이션의 정보, 리소스 접근 정보 내부 이벤트 전달 및 구독 방법 등을 담고 있는 오브젝트
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-		// 클래스 정보만 넘긴다.
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();  // 생략하면 안됨
+
+				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this)
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
-		// 가지고 있는 구성정보로 컨테이너를 초기화하기 위해서 refresh 호출
 		applicationContext.refresh();
 
 
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(applicationContext) // DispatcherSevlet 에 컨테이너 정보들을 넘겨줘야하는데 applicationContext를 사용한다.
-			).addMapping("/*");	// 모든 요청을 다 처리한다.
-		});
-		webServer.start();
+
 
 	}
 }
