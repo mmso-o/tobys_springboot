afd49ca2312e3ef5817440d339c2897f05207e68
mmso-o <4.mmsoo@gmail.com>
2023-02-19 14:54:09 +0900

    feat: Bean 등록을 통한 Contorller 등록

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -17,9 +18,16 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		// 애플리케이션의 정보, 리소스 접근 정보 내부 이벤트 전달 및 구독 방법 등을 담고 있는 오브젝트
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		// 클래스 정보만 넘긴다.
+		applicationContext.registerBean(HelloController.class);
+		// 가지고 있는 구성정보로 컨테이너를 초기화하기 위해서 refresh 호출
+		applicationContext.refresh();
+
+
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			HelloController helloController = new HelloController();
 
 			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
@@ -27,13 +35,12 @@ public class HellobootApplication {
 					// 인증, 보안, 다국어 등 공통 기능을 처리함
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
-						String ret = helloController.hello(name);
 
-						resp.setStatus(HttpStatus.OK.value());
+						HelloController HelloController = applicationContext.getBean(HelloController.class);
+						String ret = HelloController.hello(name);
+
 						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
 						resp.getWriter().println("Hello Servlet: " + ret);
-					} else if (req.getRequestURI().equals("/user")) {
-
 					} else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
 					}
