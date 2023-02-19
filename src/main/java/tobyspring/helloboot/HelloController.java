3b6da567f7619c42ef2f40e1fc9d79bed501df9c
mmso-o <4.mmsoo@gmail.com>
2023-02-19 16:58:15 +0900

    feat: dispatcherServlet 을 사용한 코드 간결화

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,13 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
+@RequestMapping("/myapp")
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,7 +15,10 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @ResponseBody // 디폴트로는 스트링으로 리턴 시 스트링 이름의 view 가 존재하는지 찾는다. ResponseBody 어노테이션 존재 시 그대로 리턴한다.
+    @GetMapping("/hello")
     public String hello(String name) {
+
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,23 +3,14 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// 애플리케이션의 정보, 리소스 접근 정보 내부 이벤트 전달 및 구독 방법 등을 담고 있는 오브젝트
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		// 클래스 정보만 넘긴다.
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
@@ -29,24 +20,9 @@ public class HellobootApplication {
 
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안, 다국어 등 공통 기능을 처리함
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController HelloController = applicationContext.getBean(HelloController.class);
-						String ret = HelloController.hello(name);
-
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println("Hello Servlet: " + ret);
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/*");	// 모든 요청을 다 처리한다.
+			servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext) // DispatcherSevlet 에 컨테이너 정보들을 넘겨줘야하는데 applicationContext를 사용한다.
+			).addMapping("/*");	// 모든 요청을 다 처리한다.
 		});
 		webServer.start();
 
