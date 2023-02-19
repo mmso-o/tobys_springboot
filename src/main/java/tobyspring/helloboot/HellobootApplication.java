956a29850d74d3ab12f0540ae7194f06d9978fad
mmso-o <4.mmsoo@gmail.com>
2023-02-19 17:59:22 +0900

    feat: Bean 생명주기 메소드
    이건 정확하게 이해가 안됐다.
    DispatcherServlet 은 ApplicationContext 가 주입되야하는데, Aware 라는 인터페이스를 통해서 직접 구현하지 않아도 스프링이 직접 주입을 해 준다는 의미같다.

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,8 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,17 +14,28 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration // 구성정보를 가지고 있다는걸 명시하며 Configuration 이 스프링 정보 중 가장 먼저 등록됨
 @ComponentScan // Component 어노테이션이 붙은 모든 클래스를 빈으로 등록한다.
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
 	public static void main(String[] args) {
-		// 애플리케이션의 정보, 리소스 접근 정보 내부 이벤트 전달 및 구독 방법 등을 담고 있는 오브젝트
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();  // 생략하면 안됨
 
-				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//				dispatcherServlet.setApplicationContext(this);
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet
 					).addMapping("/*");
 				});
 				webServer.start();
